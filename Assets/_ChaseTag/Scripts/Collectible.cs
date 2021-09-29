89854b0d59ef5433e2f8a7544887b304d0f0c554
Jean JUILLARD <j.juillard@student.isartdigital.com>
2021-09-29 17:55:43 +0200

    Created Collectible script

---
+++ Assets/_ChaseTag/Scripts/Collectible.cs
@@ -0,0 +1,15 @@
+///-----------------------------------------------------------------
+/// Author : Jean Juillard
+/// Date : 29/09/2021 17:55
+///-----------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Com.IsartDigital.ChaseTag.ChaseTag {
+	public class Collectible : MonoBehaviour {
+	
+		private void Start(){
+			Debug.Log(typeof(Collectible).ToString());
+		}
+	}
+}
