4654a7ad2e96fc153d9fd110aab87ab3fc3cff60
Jean JUILLARD <j.juillard@student.isartdigital.com>
2021-09-30 22:27:31 +0200

    Added CollectibleManager generates a new collectible in a spawn area when another is destroyed

---
+++ Assets/_ChaseTag/Scripts/CollectibleManager.cs
@@ -0,0 +1,99 @@
+///-----------------------------------------------------------------
+/// Author : Jean Juillard
+/// Date : 30/09/2021 19:54
+///-----------------------------------------------------------------
+
+using Com.IsartDigital.ChaseTag.Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.IsartDigital.ChaseTag.ChaseTag {
+	public sealed class CollectibleManager : MonoBehaviour {
+	
+		public static CollectibleManager Instance { get; private set; }
+
+        private List<Collectible> collectibles = new List<Collectible>();
+
+        [SerializeField] private Collectible collectiblePrefab = default;
+        
+        [SerializeField] private float collectibleRespawnDelay = 0f;
+        private WaitForSeconds respawnDelay;
+
+        [SerializeField] private int numSimultaneousCollectibles = 1;
+
+        [Tooltip("make sure both spawnZoneBottomLeftAngle and spawnZoneTopRightAngle have the same Y coordinate")]
+        [SerializeField] private Transform spawnZoneBottomLeftAngle = default;
+        [Tooltip("make sure both spawnZoneBottomLeftAngle and spawnZoneTopRightAngle have the same Y coordinate")]
+        [SerializeField] private Transform spawnZoneTopRightAngle = default;
+
+        [SerializeField] private string groundTag = "Ground";
+
+		private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+            {
+                Debug.LogWarning("Trying to create multiple instances of singleton script, creation denied");
+                Destroy(gameObject);
+            }
+
+            Collectible.OnCollected += Collectible_OnCollected;
+            respawnDelay = new WaitForSeconds(collectibleRespawnDelay);
+        }
+
+        private void Collectible_OnCollected(Collectible collectible)
+        {
+            collectibles.Remove(collectible);
+
+            Debug.Log("spawn new collectible");
+            StartCoroutine(SpawnNewCollectible());
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this)
+                Instance = null;
+
+            Collectible.OnCollected -= Collectible_OnCollected;
+
+            DestroyAllCollectibles();
+        }
+
+        public void DestroyAllCollectibles()
+        {
+            int length = collectibles.Count;
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                Destroy(collectibles[i]);
+
+                collectibles.RemoveAt(i);
+            }
+        }
+
+        private IEnumerator SpawnNewCollectible()
+        {
+            yield return respawnDelay;
+
+            Collectible collectible = Instantiate(collectiblePrefab);
+            Vector3 position = Vector3.zero;
+
+            RaycastHit hit;
+            bool positionIsValid = false;
+
+            while (!positionIsValid)
+            {
+                position = RandomTool.GetRandomPositionInCube(spawnZoneBottomLeftAngle.position, spawnZoneTopRightAngle.position);
+                
+                if (Physics.Raycast(position, Vector3.down, out hit, position.y + 0.5f))
+                {
+                    positionIsValid = hit.collider.CompareTag(groundTag);
+                }
+            }
+
+            collectible.transform.position = position;
+        }
+	}
+}
