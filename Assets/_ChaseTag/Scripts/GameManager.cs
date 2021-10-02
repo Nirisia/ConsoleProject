4b27671751d028070af5708e9f15e599549fb23b
Jean JUILLARD <j.juillard@student.isartdigital.com>
2021-10-02 16:00:01 +0200

    Added Win conditions in GameManager

--- Assets/_ChaseTag/Scripts/GameManager.cs
+++ Assets/_ChaseTag/Scripts/GameManager.cs
@@ -3,13 +3,17 @@
 /// Date : 02/10/2021 15:52
 ///-----------------------------------------------------------------
 
+using Com.IsartDigital.Common;
 using UnityEngine;
 
 namespace Com.IsartDigital.ChaseTag.ChaseTag {
 	public sealed class GameManager : MonoBehaviour {
 	
 		public static GameManager Instance { get; private set; }
-		
+
+        [SerializeField] private Timer timerPrefab = default;
+        public Timer GameTimer { get; private set; }
+
 		private void Awake()
         {
             if (Instance == null)
@@ -26,5 +30,42 @@ namespace Com.IsartDigital.ChaseTag.ChaseTag {
             if (Instance == this)
                 Instance = null;
         }
-	}
+
+        private void Start()
+        {
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            GameTimer = Instantiate(timerPrefab);
+            GameTimer.Init(300);
+            GameTimer.OnTimerCompleted += GameTimer_OnTimerCompleted;
+
+            Player.OnMouseCaught += Player_OnMouseCaught;
+        }
+
+        private void Player_OnMouseCaught(Player player)
+        {
+            Player.OnMouseCaught -= Player_OnMouseCaught;
+
+            //player wins
+        }
+
+        private void GameTimer_OnTimerCompleted()
+        {
+            GameTimer.OnTimerCompleted -= GameTimer_OnTimerCompleted;
+
+            Player winner;
+
+            if (PlayerManager.Instance.TryGetMousePlayer(out winner))
+            {
+                //winner wins
+            }
+            else
+            {
+                //tie
+            }
+        }
+    }
 }
